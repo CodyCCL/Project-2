705592fa2443fb5bd368ec5184492b27c45b4365
Joseph Browning <j.browning527@gmail.com>
2023-12-04 20:47:01 -0500

    created initial food handlebars framework

--- controllers/page-routes.js
+++ controllers/page-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { Food } = require('../models');
 
 router.get('/', (req, res) => {
   res.render('login');
@@ -12,9 +13,20 @@ router.get('/exercisesets', (req, res) => {
   res.render('exercisesets');
 });
 
-router.get('/food', (req, res) => {
-  res.render('food');
+router.get('/food', async (req, res) => {
+  try {
+    const dbFoodData = await Food.findAll({});
+    const foodData = dbFoodData.map(item => item.get({ plain: true })); // Convert each object to plain data
+    res.render('Food', { foodData });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
+// router.get('/food', (req, res) => {
+
+//   res.render('food');
+// });
 
 router.get('/usergoals', (req, res) => {
   res.render('usergoals');
