6fe7d29153b1e2dcd112c5fd68541943e2ad7d8e
Joseph Browning <j.browning527@gmail.com>
2023-12-06 19:49:19 -0500

    updated index in api routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
-const foodRoutes = require('./food');
-const exerciseRoutes = require('./exercises');
+// const foodRoutes = require('./food');
+// const exerciseRoutes = require('./exercises');
 
 router.use('/users', userRoutes);
-router.use('/food', foodRoutes);
-router.use('/exercises', exerciseRoutes)
+// // router.use('/food', foodRoutes);
+// router.use('/exercises', exerciseRoutes)
 
 module.exports = router;
