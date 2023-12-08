ef317debe00e06c53ca3f0af585820ed83add24d
Joseph Browning <j.browning527@gmail.com>
2023-12-07 19:52:04 -0500

    fixed seed bug

--- seeds/exerciseData.js
+++ seeds/exerciseData.js
@@ -1,6 +1,6 @@
-const { ExerciseSets } = require("../models");
+const { Exercise } = require("../models");
 
-const exerciseSetData = [];
+const exerciseData = [];
 
 let date = new Date(2023, 10, 30); // 11-30-2023
 
@@ -9,20 +9,16 @@ for (let i = 1; i <= 20; i++) {
     const entryDate = new Date(date);
     entryDate.setDate(date.getDate() + dayOffset);
 
-    const numberOfSets = (i % 3) + 1; // 1-3 sets
-    for (let j = 1; j <= numberOfSets; j++) {
-        exerciseSetData.push({
-            SetNumber: j, // Unique identifier for each set
-            ExerciseId: i, // Foreign key linking to the Exercise
-            RepCount: 10 * j, // 10, 20, 30 reps
-            RepGoal: 10 * j,
-            EntryDate: entryDate.toISOString().split('T')[0], // Format date to YYYY-MM-DD
-            Weight: i % 2 === 0 ? 5 * j : 0, // Weight for Strength exercises
-            User: 1 // Assuming a user_id of 1 for all entries
-            // Add other fields if necessary
-        });
-    }
+    exerciseData.push({
+        ExerciseId: i, // Assuming ExerciseId is still needed as a unique identifier
+        Name: `Exercise ${i}`,
+        Type: i % 2 === 0 ? 'Strength' : 'Cardio', // Alternating between 'Cardio' and 'Strength'
+        Calories: 50 * (i % 3), // 0, 50, or 100 calories
+        EntryDate: entryDate.toISOString().split('T')[0], // Format date to YYYY-MM-DD
+        User: 1 // Assuming a user_id of 1 for all entries
+    });
 }
-const seedExceriseSet = () => ExerciseSets.bulkCreate(exerciseSetData);
 
-module.exports = seedExceriseSet;
+const seedExcerise = () => Exercise.bulkCreate(exerciseData);
+
+module.exports = seedExcerise;
